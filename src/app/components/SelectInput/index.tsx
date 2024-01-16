005d688b965096e04d8ba6b302549c8691f3128b
bruna <brunadafonseca@gmail.com>
2024-01-16 16:44:30 +0100

    feat: SelectInput

---
+++ src/app/components/SelectInput/index.tsx
@@ -0,0 +1,51 @@
+import { ComponentPropsWithoutRef } from "react";
+import cx from "classnames";
+import Select from "react-select";
+
+export type SelectItem = {
+  label: string;
+  value: string;
+};
+
+type SelectInputProps = {
+  label?: string;
+  layout?: "horizontal" | "vertical";
+  size?: "sm" | "md";
+  options: SelectItem[];
+  onChange(value: any): void;
+} & Pick<
+  ComponentPropsWithoutRef<"select">,
+  "name" | "id" | "className" | "defaultValue" | "value"
+>;
+
+export const SelectInput = ({
+  id,
+  layout = "horizontal",
+  size = "md",
+  label,
+  options,
+  ...inputProps
+}: SelectInputProps) => {
+  return (
+    <div
+      className={cx(
+        "flex",
+        layout === "vertical" ? "gap-2 items-center" : "flex-col gap-1"
+      )}
+    >
+      {label && (
+        <label
+          htmlFor={id}
+          className={cx(
+            "text-secondary-100",
+            size === "sm" ? "text-sm" : "text-md"
+          )}
+        >
+          {label}
+        </label>
+      )}
+
+      <Select isMulti id={id} {...inputProps} />
+    </div>
+  );
+};
